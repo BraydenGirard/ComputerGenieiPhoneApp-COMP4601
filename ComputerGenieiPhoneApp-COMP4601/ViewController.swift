5372a33e3f4113ff5a0baf3ee995e74f1640d65c
BraydenGirard <braydengirard@icloud.com>
2015-03-28 15:18:03 -0400

    Added user class

---
+++ ComputerGenieiPhoneApp-COMP4601/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComputerGenieiPhoneApp-COMP4601
+//
+//  Created by Brayden Girard on 2015-03-28.
+//  Copyright (c) 2015 Brayden Girard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
